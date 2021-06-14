faefad2c9ad0ace23d00d3a139417bd9a1f05b21
sk59152 <sk59152@MKD-CORP-BCP002.CORP.CYIENT.COM>
2021-06-14 08:57:46 +0530

    Second commit

--- src/test/java/com/cyient/utilities/DataProviderUtiles.java
+++ src/test/java/com/cyient/utilities/DataProviderUtiles.java
@@ -7,24 +7,34 @@ import com.cyient.base.WebDriverWrapper;
 
 public class DataProviderUtiles extends WebDriverWrapper {
 		
-	@Test(dataProvider="LoginData")
-	public void loginTest(String user, String pwd, String exp)
-	{
-		System.out.println(user+" "+pwd+" "+exp);
-		
-	}
+	
 	
 	@DataProvider(name="LoginData")
-	public String[][] getData()
+	public Object[][] getData()
 	{
-		String loginData[][]= {
-				{"7582859175","Kum@12345","Valid"},
-				{"7882859175","Kum@12345","Invalid"},
-				{"7582859175","Kum@1234", "Invalid"},
-				{"7282859175","Kum@1234","Invalid"}
-				
-		};
-		return loginData;
+		Object[][] result =new Object[4][2];
+		 
+		result[0][0]="7582859175";
+		result[0][1]="Sur@123456789";
+		
+		result[1][0]="7382859175";
+		result[1][1]="Sur@123456789";
+		
+		result[2][0]="7582859175";
+		result[2][1]="sur@123456789";
+		
+		result[3][0]="7382859175";
+		result[3][1]="sur@123456789";
+		
+		return result;
+		
+		
+	}
+	@Test(dataProvider="LoginData")
+	public void loginTest(String user, String pwd)
+	{
+		System.out.println(user+" "+pwd);
+		
 	}
 	
 
